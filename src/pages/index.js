71b45f9af2049c72395d8ba7bfa64ec9a846ba7b
Kevin Laminto <kevin.laminto@gmail.com>
2020-09-28 12:26:45 +1000

    Init

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,25 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+
+import Image from "../components/Image"
 
 function IndexPage() {
-  return (
-    <Layout>
-      <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="Home"
-      />
+    return (
+        <Layout>
+            <SEO
+                keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+                title="Home"
+            />
 
-      <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
+            <Image imageName="wave.jpg" className="mb-10" />
+            <p className="text-justify">
+                The Great Wave off Kanagawa (Japanese: 神奈川沖浪裏, Hepburn: Kanagawa-oki Nami Ura, "Under the Wave off Kanagawa"), also known as The Great Wave or simply The Wave, is a woodblock print by the Japanese ukiyo-e artist Hokusai. It was published sometime between 1829 and 1833[1] in the late Edo period as the first print in Hokusai's series Thirty-six Views of Mount Fuji. It is Hokusai's most famous work and is often considered the most recognizable work of Japanese art in the world.
+                The image depicts an enormous wave threatening three boats off the coast in the Sagami Bay (Kanagawa Prefecture) while Mount Fuji rises in the background. Sometimes assumed to be a tsunami, the wave is more likely to be a large rogue wave.[2] As with the other prints in the series, it depicts the area and activities around Mount Fuji under varying conditions. Throughout the series are dramatic uses of Berlin blue pigment.
+            </p>
 
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
-        </p>
-      </section>
-    </Layout>
-  );
+        </Layout>
+    );
 }
 
 export default IndexPage;
